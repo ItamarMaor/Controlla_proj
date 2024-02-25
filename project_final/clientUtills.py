5f55d0d7067ce05c789a6967c9e5a553fbe5bf9c
ItamarMaor <itamar.maor18@gmail.com>
2024-02-25 18:55:13 +0200

    Remove unnecessary files and code

--- project_final/clientUtills.py
+++
@@ -1,8 +0,0 @@
-import threading
-import os
-
-
-def computer_shutdown(client_socket):
-    # If the received message is shutdown the client does so 
-    os.system("shutdown /s /t 15")
-    client_socket.close()
